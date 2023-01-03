851574d2560ccd3a98da88edd05e781fd4ea4fb0
Zaya <zayadeveloper8@gmail.com>
2023-01-03 16:03:11 -0600

    add console

--- src/App.js
+++ src/App.js
@@ -3,6 +3,36 @@ import {About, Footer, Header, Skills, Testimonial, Work} from './container/inde
 import {Navbar} from './components'
 import './App.scss'
 const App = () => {
+  console.log(
+
+     "     ._       __          ____                              \n"+
+     "    ';  `\--,-' /`)    _.-'    `-._                          \n"+
+     "     \_/    ' | /`--,'            `-.     .--....____       \n"+
+     "     /                              `._.'           `---... \n"+
+     "    |-.   _      ;    meowww.            .-----..._______)  \n"+
+     "   ,,\q/ (q_>'_...                      .-'                 \n"+
+     "   ===/ ; _.-'~~-             /       ,'                    \n"+
+     "   ````-'_,:  `..         ___(       |                      \n"+
+     "            \         : /'/   \      \                      \n"+
+     "             `.      //' (    :`    `\                      \n"+
+     "             / \    :     `  /  `-.  /                      \n"+
+ "            (  (;   ;     (__/  /  /                          \n"+
+ "             \,_)\  ;          ,'  /                           \n"+
+ "    .-.          |  |         `--                             \n"+
+ "   (''_.)-._     (__,>                                          \n"+
+ "                                                                \n"+
+ "       Hey there 👋, curious!\n"+
+ "       You're probably wondering how cool this site is.\n"+
+ "                                                          \n"+
+ "       If you think it's interesting and want to see the code behind it, \n"+
+ "       I've placed it in the open at https://github.com/WEDNESDAYfate/ZAYA-portfolio \n"+
+ "       Feel free to check it out!\n "+
+ "                                  \n "+
+ "      Also, you can contact me on https://www.instagram.com/wednesdayfate/ or zayadeveloper8@gmail.com ✨.\n"
+
+
+  );
+
   return (
     <div className='app'>
         <Navbar/>
